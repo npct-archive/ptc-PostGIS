21670fa67dad448068fa0176f4cacb063f4b0302
Malcolm <malcolmmorgan02@hotmail.com>
2016-08-23 18:12:27 +0100

    Update Connect.R
    Updated askDB fucntion to disconnect form the DB after completing the
    request

--- connect.R
+++ connect.R
@@ -1,10 +1,29 @@
+# contains a function which connects to the database
+# runs SQL query and disconnects
+# this is to keep the SQL code a clean as possbile
+# in the rest of the code. I alos makes sure that the
+# DB is not overloaded with unclosed connections
+
+#function 
+askDB <- function (sql){
+  
 # Variables for Database connection
-dbname <- "dbname"
-host <- "host"
-user <- "user"
-port <- 5432
-password <- "password"
+dbname <- ""
+host <- ""
+user <- ""
+port <- ""
+password <- ""
+
 # Connect to Database
-con <- dbConnect(PostgreSQL(), dbname = dbname, user = user,
-                 host = host,
-                 password = password)
+con <- dbConnect(dbDriver("PostgreSQL"), dbname = dbname, user = user, host = host, password = password)
+
+# Query the database
+
+result <- get_postgis_query(con, sql, geom_name = "geom", hstore_name = NA_character_)
+
+#disconnect from the database
+dbDisconnect(con)
+
+#Return the result
+return(result)
+}
