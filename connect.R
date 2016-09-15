46a71bdddf3fcb257ae96bd9f68f23473763d303
Malcolm <malcolmmorgan02@hotmail.com>
2016-09-15 18:28:50 +0100

    Connect.R
    New username and password that are read only so can be made available

--- connect.R
+++ connect.R
@@ -1,52 +1,11 @@
-# contains a function which connects to the database
-# runs SQL query and disconnects
-# this is to keep the SQL code a clean as possbile
-# in the rest of the code. I alos makes sure that the
-# DB is not overloaded with unclosed connections
-
-# get libraries and passwords
-source("set-up.R")
+#Pool Databse connection
 
 
-#askDB_all - connect get info then disconnect - a basic function for connecting to the DB
-askDB_all <- function (sql){
-  
-  # Connect to Database
-  con <- dbConnect(dbDriver("PostgreSQL"), dbname = dbname, user = user, host = host, password = password)
 
-  # Query the database
-  result <- get_postgis_query(con, sql, geom_name = "geom", hstore_name = NA_character_)
 
-  #disconnect from the database
-  dbDisconnect(con)
-
-  #Return the result
-  return(result)
-}
-
-conDB <- function(){
-  #Connect to Database
-  con <- dbConnect(dbDriver("PostgreSQL"), dbname = dbname, user = user, host = host, password = password)
-}
-
-#askDB - connect get info but don't disconnect - for when you are asking several things at once
-askDB <- function (sql){
-  # Connect to Database
-  con <- dbConnect(dbDriver("PostgreSQL"), dbname = dbname, user = user, host = host, password = password)
-  
-  # Query the database
-  result <- get_postgis_query(con, sql, geom_name = "geom", hstore_name = NA_character_)
-  
-  #disconnect from the database
-  dbDisconnect(con)
-  
-  #Return the result
-  return(result)
-}
-
-#disDB - disconnect tfrom DB
-disDB <- function (){
-
-  #disconnect from the database
-  dbDisconnect(con)
-}
+# Variables for Database connection
+dbname <- "ptc_db"
+host <- "ptc-instance2.cehoy1ckliol.eu-west-1.rds.amazonaws.com"
+user <- "ptc_user" #read user
+port <- "5432"
+password <- "nSsjA3D6e2IJWsxEiBF5" #read password
